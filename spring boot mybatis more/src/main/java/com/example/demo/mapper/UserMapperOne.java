812ebc512e0b84e8c47094e1441244d49e866b7e
mangixs <fnhdcwy@hotmail.com>
2019-11-29 16:36:21 +0800

    deletemybatismore

--- spring boot mybatis more/src/main/java/com/example/demo/mapper/UserMapperOne.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.mapper;
-
-import com.example.demo.dto.User;
-
-import java.util.List;
-
-public interface UserMapperOne {
-    List<User> getAllUser();
-}
--- spring boot mybatis more/src/main/java/com/example/demo/mapper2/UserMapperTwo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.mapper2;
-
-import com.example.demo.dto.User;
-
-import java.util.List;
-
-public interface UserMapperTwo {
-    List<User> getAllUser();
-}
